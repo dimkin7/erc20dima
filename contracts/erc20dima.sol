208e61649cb53899703331bf33856757feec99b3
dimkin7 <dima-besh@yandex.ru>
2022-02-25 22:04:20 +0300

    Написан код и тесты.

--- contracts/erc20dima.sol
+++ contracts/erc20dima.sol
@@ -2,32 +2,45 @@
 // токен стандарта ERC-20
 // функции взяты из https://eips.ethereum.org/EIPS/eip-20
 
-//TODO использовать env
-
-
 pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
 
 contract ERC20Dima {
     string private mToken = "DimaToken";
     string private mSymbol = "DIT";
     uint8 private mDecimals = 18;
+    address private mContractOwner;
+    uint256 private mTotal;
 
     //хранение баланса
     mapping(address => uint256) private mBalance;
 
     //хранение allowance
-    struct AllowanceStruc{
+    struct AllowanceStruc {
         address spender;
         uint256 remaining;
     }
     mapping(address => AllowanceStruc[]) private mAllowance;
 
+    ///////////  Events
+    //MUST trigger when tokens are transferred, including zero value transfers.
+    //A token contract which creates new tokens SHOULD trigger a Transfer event with the _from address set to 0x0 when tokens are created.
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    //MUST trigger on any successful call to approve(address _spender, uint256 _value).
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     //конструктор
     constructor(uint256 initialSupply) {
-        _mint(msg.sender, initialSupply);
+        mContractOwner = msg.sender;
+        //передаем все токены владельцу контракта
+        mint(msg.sender, initialSupply);
     }
-
 
     //////// View functions /////////////////
 
@@ -47,63 +60,236 @@ contract ERC20Dima {
     }
 
     //Returns the account balance of another account with address _owner.
-    function balanceOf(address _owner) public view returns (uint256 balance)
-    {
+    function balanceOf(address _owner) public view returns (uint256 balance) {
         return mBalance[_owner];
     }
 
     //Returns the total token supply.
-    function totalSupply() public view returns (uint256)
-    {
-        return 33; //TODO
-    }   
+    function totalSupply() public view returns (uint256) {
+        return mTotal;
+    }
 
     //Returns the amount which _spender is still allowed to withdraw from _owner.
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining)
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256 remaining)
     {
         remaining = 0;
 
         //получаем разрешения _owner
         AllowanceStruc[] memory ownerAllowance = mAllowance[_owner];
         //ищем резрешение для _spender
-        for(uint i = 0; i < ownerAllowance.length; i++)
-        {
-            if (ownerAllowance[i].spender == _spender){
+        for (uint256 i = 0; i < ownerAllowance.length; i++) {
+            if (ownerAllowance[i].spender == _spender) {
                 //если нашли _spender - возвращаем
                 remaining = ownerAllowance[i].remaining;
                 return remaining;
             }
         }
-        
+
         return remaining;
     }
 
     //Functions (non view)
 
-    //transfer
-    function transfer(address _to, uint256 _value) public returns (bool success){
-
+    //Transfers _value amount of tokens to address _to, and MUST fire the Transfer event.
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        success = transferFrom(msg.sender, _to, _value);
     }
-    //function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
-    //function approve(address _spender, uint256 _value) public returns (bool success)
 
-    //increaseAllowance
-    //decreaseAllowance
+    //Transfers _value amount of tokens from address _from to address _to, and MUST fire the Transfer event.
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        success = false;
+        //console.log("transferFrom:", _from, _to, _value);
+        //console.log("msg.sender:", msg.sender);
+        require(_from != address(0), "Sender cannot be 0.");
+        require(_to != address(0), "Receiver cannot be 0.");
 
-    //event Transfer(address indexed _from, address indexed _to, uint256 _value)
-    //event Approval(address indexed _owner, address indexed _spender, uint256 _value)
+        //проверяем достаточное кол-во токенов на балансе
+        require(_value <= mBalance[_from], "Not enough tokens.");
 
+        //если не со своего счета, проверяем разрешение и уменьшаем
+        if (_from != msg.sender) {
+            success = decreaseAllowanceFrom(_from, msg.sender, _value);
 
+            //The function SHOULD throw unless the _from account has deliberately authorized the sender of the message via some mechanism.
+            require(success == true, "DecreaseAllowance = false");
+        }
 
-    //Добавить функции mint и burn TODO
+        //передача
+        mBalance[_from] -= _value;
+        mBalance[_to] += _value;
+
+        //событие
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+    //Allows _spender to withdraw from your account multiple times, up to the _value amount. If this function is called again it overwrites the current allowance with _value.
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        success = false;
+        require(_spender != address(0), "Spender cannot be 0.");
+
+        //получаем разрешения msg.sender
+        //AllowanceStruc[] memory ownerAllowance = mAllowance[msg.sender]; - так было изначально
+
+        //ищем резрешение для _spender и обновляем
+        for (uint256 i = 0; i < mAllowance[msg.sender].length; i++) {
+            if (mAllowance[msg.sender][i].spender == _spender) {
+                mAllowance[msg.sender][i].remaining = _value;
+
+                //событие
+                emit Approval(msg.sender, _spender, _value);
+
+                return true;
+            }
+        }
+
+        //если не нашли - добавляем
+        AllowanceStruc memory newAllowance;
+        newAllowance.spender = _spender;
+        newAllowance.remaining = _value;
+        mAllowance[msg.sender].push(newAllowance);
+
+        //событие
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    //Atomically increases the allowance granted to spender by the caller.
+    function increaseAllowance(address _spender, uint256 _addedValue)
+        public
+        returns (bool success)
+    {
+        success = changeAllowanceCommon(
+            msg.sender,
+            _spender,
+            _addedValue,
+            true
+        );
+    }
+
+    //внутренняя - используется в decreaseAllowance и в transferFrom
+    function decreaseAllowanceFrom(
+        address _owner,
+        address _spender,
+        uint256 _subtractedValue
+    ) internal returns (bool success) {
+        success = false;
+        //console.log(            "decreaseAllowanceFrom:",            _owner,            _spender,            _subtractedValue        );
+
+        //проверяем достаточное кол-во токенов в разрешении
+        uint256 remaining = allowance(_owner, _spender);
+        require(_subtractedValue <= remaining, "Not enough allowance.");
+
+        success = changeAllowanceCommon(
+            _owner,
+            _spender,
+            _subtractedValue,
+            false
+        );
+    }
+
+    //Atomically decreases the allowance granted to spender by the caller.
+    function decreaseAllowance(address _spender, uint256 _subtractedValue)
+        public
+        returns (bool success)
+    {
+        success = decreaseAllowanceFrom(msg.sender, _spender, _subtractedValue);
+    }
+
+    //для вызова из increaseAllowance / decreaseAllowance
+    function changeAllowanceCommon(
+        address _owner,
+        address _spender,
+        uint256 _changeValue,
+        bool _increase
+    ) internal returns (bool success) {
+        success = false;
+
+        //console.log("changeAllowanceCommon:", _owner, _spender, _changeValue);
+
+        //spender cannot be the zero address.
+        require(_spender != address(0), "Spender cannot be 0.");
+        require(_owner != address(0), "Owner cannot be 0.");
+
+        //получаем разрешения _owner
+        //AllowanceStruc[] memory ownerAllowance = mAllowance[_owner]; - не получаем, а сразу используем, комментарий оставил для понимания
+        //console.log("mAllowance[_owner].length:", mAllowance[_owner].length);
+
+        //ищем резрешение для _spender
+        for (uint256 i = 0; i < mAllowance[_owner].length; i++) {
+            if (mAllowance[_owner][i].spender == _spender) {
+                if (_increase == true) {
+                    mAllowance[_owner][i].remaining += _changeValue;
+                } else {
+                    mAllowance[_owner][i].remaining -= _changeValue;
+                }
+
+                //Emits an Approval event indicating the updated allowance.
+                emit Approval(
+                    _owner,
+                    _spender,
+                    mAllowance[_owner][i].remaining
+                );
+                //console.log("changeAllowanceCommon: return true");
+                return true;
+            }
+        }
+    }
+
+    //Добавить функции mint и burn
 
     //Creates amount tokens and assigns them to account, increasing the total supply.
-    function _mint(address account, uint256 amount) internal
+    function mint(address _account, uint256 _amount)
+        public
+        returns (bool success)
     {
+        success = false;
+        require(msg.sender == mContractOwner, "Only for contract owner.");
 
-        //TODO
-        //TODO   Emits a transfer event with from set to the zero address.
+        //account cannot be the zero address.
+        require(_account != address(0), "Account cannot be 0.");
 
+        mBalance[_account] += _amount;
+        mTotal += _amount;
+        success = true;
 
+        //Emits a transfer event with from set to the zero address.
+        emit Transfer(address(0), _account, _amount);
+    }
+
+    //Destroys amount tokens from account, reducing the total supply.
+    function burn(address _account, uint256 _amount)
+        public
+        returns (bool success)
+    {
+        success = false;
+        require(msg.sender == mContractOwner, "Only for contract owner.");
+
+        //account cannot be the zero address.
+        require(_account != address(0), "Account cannot be 0.");
+        //account must have at least amount tokens.
+        require(mBalance[_account] >= _amount, "Not enough tokens.");
+
+        mBalance[_account] -= _amount;
+        mTotal -= _amount;
+        success = true;
+
+        //Emits a transfer event with to set to the zero address.
+        emit Transfer(_account, address(0), _amount);
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,35 +1,124 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { Contract } from "ethers";
-//import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 //import { ERC20Dima__factory, ERC20Dima } from "../typechain";
-import type { Signer } from "ethers";
+//import type { Signer } from "ethers";
+
+//по мотивам
+//https://yuichiroaoki.medium.com/testing-erc20-smart-contracts-in-typescript-hardhat-9ad20eb40502
 
 describe("ERC20Dima", function () {
-  //before  TODO
-  
   let erc20: Contract;
-  let owner: Signer;
-	let addr1: Signer;
-	let addr2: Signer;
-	
+  let owner: SignerWithAddress;
+	let addr1: SignerWithAddress;
+	let addr2: SignerWithAddress;
+	const addrZero = "0x0000000000000000000000000000000000000000";
+
+
   //beforeEach передеплой контракта
   beforeEach(async function () {
     [owner, addr1, addr2] = await ethers.getSigners();
 
     const factoryERC20Dima = await ethers.getContractFactory("ERC20Dima") ;
-    erc20 = await factoryERC20Dima.deploy(33);
+    erc20 = await factoryERC20Dima.deploy(1000);
     await erc20.deployed();
   });
 
 
-  it("ERC20 test", async function () {
+  it("ERC20 names", async function () {
 
     expect(await erc20.name()).to.equal("DimaToken");
     expect(await erc20.symbol()).to.equal("DIT");
+    expect(await erc20.decimals()).to.equal(18);
+  });
 
-  
+  it("ERC20 balance", async function () {
+    expect( await erc20.balanceOf(owner.address) ).to.equal(1000);
 
+    //увеличим баланс
+    await erc20.mint(owner.address, 1000);
+    expect( await erc20.balanceOf(owner.address) ).to.equal(2000);
+    //уменьшим
+    await erc20.burn(owner.address, 500);
+    expect( await erc20.balanceOf(owner.address) ).to.equal(1500);
+    //итого
+    expect( await erc20.totalSupply() ).to.equal(1500);
+  });
+
+  //передача
+  it("ERC20 transfer", async function () {
+    await erc20.transfer(addr1.address, 200);
+    expect( await erc20.balanceOf(addr1.address) ).to.equal(200); //получатель
+    expect( await erc20.balanceOf(owner.address) ).to.equal(800); //я
+    expect( await erc20.totalSupply() ).to.equal(1000); //итого
+  });
+
+  //разрешение
+  it("ERC20 approve", async function () {
+    //console.log("owner:" + owner.address); 
+    //console.log("addr1:" + addr1.address); 
+    //console.log("addr2:" + addr2.address); 
+    
+    expect( await erc20.allowance(owner.address, addr1.address) ).to.equal(0);  //разрешено 0
+    await erc20.approve(addr1.address, 200);
+    await erc20.approve(addr2.address, 20); //для тестирования if
+    expect( await erc20.allowance(owner.address, addr1.address) ).to.equal(200);  //разрешено 200
+    expect( await erc20.allowance(owner.address, addr2.address) ).to.equal(20);  //второму разрешено 20
+
+    // We use .connect(signer) to send a transaction from another account
+    await erc20.connect(addr1).transferFrom(owner.address, addr2.address, 50); //передает 50
+    
+    expect(  erc20.connect(addr1).transferFrom(owner.address, addr2.address, 160)).to.be.revertedWith("Not enough allowance."); //пытаемся 160
+
+
+    expect( await erc20.balanceOf(owner.address) ).to.equal(950);  //я
+    expect( await erc20.balanceOf(addr2.address) ).to.equal(50); //2-й
+    expect( await erc20.allowance(owner.address, addr1.address) ).to.equal(150);  //разрешено 150
+
+    await erc20.approve(addr1.address, 10); //сбрасываем на 10
+    expect( await erc20.allowance(owner.address, addr1.address) ).to.equal(10);  //разрешено 10
+
+    await erc20.increaseAllowance(addr1.address, 20); //добавляем 20
+    expect( await erc20.allowance(owner.address, addr1.address) ).to.equal(30);  //разрешено 30
+
+    await erc20.decreaseAllowance(addr1.address, 5); //убавляем 5
+    expect( await erc20.allowance(owner.address, addr1.address) ).to.equal(25);  //разрешено 25
+
+    await erc20.increaseAllowance(addr2.address, 20); //второму добавляем 20 - для тестирования if
+    expect( await erc20.allowance(owner.address, addr2.address) ).to.equal(40);  //разрешено 40
 
   });
+
+  //тест исключительных ситуаций
+  it("revert 1", async function () {
+    await erc20.approve(addr1.address, 2000); //можно разрешить 2000, даже если на балансе 1000, вдруг позже появятся
+    expect(  erc20.connect(addr1).transferFrom(owner.address, addr2.address, 2000)).to.be.revertedWith("Not enough tokens."); //пытаемся 2000, на балансе 1000
+    expect(  erc20.connect(addr1).transferFrom(owner.address, addrZero, 200)).to.be.revertedWith("Receiver cannot be 0."); //адрес 0
+    expect(  erc20.connect(addr1).transferFrom(addrZero, addr2.address, 200)).to.be.revertedWith("Sender cannot be 0."); //адрес 0
+  });
+
+  it("revert 2", async function () {
+    //создавать mint / уничтожать burn  только владелец
+    expect(  erc20.connect(addr1).mint(owner.address, 10)).to.be.revertedWith("Only for contract owner."); 
+    expect(  erc20.connect(addr1).burn(owner.address, 10)).to.be.revertedWith("Only for contract owner."); 
+
+    expect(  erc20.mint(addrZero, 10)).to.be.revertedWith("Account cannot be 0."); 
+    expect(  erc20.burn(addrZero, 10)).to.be.revertedWith("Account cannot be 0."); 
+    expect(  erc20.burn(owner.address, 2000)).to.be.revertedWith("Not enough tokens."); 
+
+    expect(  erc20.increaseAllowance(addrZero, 10)).to.be.revertedWith("Spender cannot be 0."); 
+
+    expect(  erc20.approve(addrZero, 2000)).to.be.revertedWith("Spender cannot be 0."); //адрес 0
+  });
+
+
+  it("allowance 2", async function () {
+    //сначала нужно approve
+    await erc20.increaseAllowance(addr2.address, 20); //добавляем 20
+    expect( await erc20.allowance(owner.address, addr2.address) ).to.equal(0);  
+  });
+    
+
 });
+
